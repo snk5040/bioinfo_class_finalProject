b42bee07a41d1740e9cb55a37446b3a94b0d46cc
snk5040 <41748925+snk5040@users.noreply.github.com>
2022-05-12 20:49:52 -0400

    Search Functionality
    Finally added search functionality without bugs.  Searches generate cards at the end of the card stack.

--- code/get_sql.cgi
+++ code/get_sql.cgi
@@ -6,27 +6,29 @@
 import cgi, json
 import os
 import mysql.connector
-import random
 
 # Odorbase is a 128 entry, 6 column SQL table on the class server with flybase genes related to olfaction
-# This script pulls a random entry from the database and dumps the data in JSON format
+# This script pulls a specific entry from the database and dumps the data in JSON format
 
 def main():
 
     print("Content-Type: application/json\n\n")
+    form = cgi.FieldStorage()
+    term = form.getvalue('search_term')
 
     conn = mysql.connector.connect(user='skyllma1', password='password2', host='localhost', database='skyllma1')
     curs = conn.cursor()
 
     qry = """
             SELECT FBid, feature_type, name, symbol, uniprot_function, protein_family FROM odorbase
-            WHERE ind = %s
+            WHERE FBid LIKE %s OR name LIKE %s OR symbol LIKE %s OR uniprot_function LIKE CONCAT('%', %s, '%') OR protein_family LIKE CONCAT('%', %s, '%')
     """
-    random_index = random.randrange(0,129)
-    curs.execute(qry, (random_index, ))
-    results = dict()
+
+    curs.execute(qry, (term, term, term, term, term))
+    results = { 'match_count': 0, 'matches': list() }
     for (FBid, feature_type, name, symbol, uniprot_function, protein_family) in curs:
-        results = {"FlyBase ID": FBid, "Feature Type":feature_type, "Name":name, "Symbol":symbol, "UniProt Function":uniprot_function, "Protein Family":protein_family}
+        results['matches'].append({"FlyBaseID": FBid, "Feature_Type":feature_type, "Name":name, "Symbol":symbol, "UniProt_Function":uniprot_function, "Protein_Family":protein_family})
+        results['match_count'] += 1
 
     curs.close()
     conn.close()
--- code/js/backend.js
+++ code/js/backend.js
@@ -8,8 +8,11 @@ $(document).ready( function() {
     $.getJSON('./get_random_sql.cgi', function(data) {
       //console.log(data); //Debug
         $.each( data, function( key, val ) {
-          $('.current-card').append('<li>'+val+'</li>');
+          $('.current-card').append('<li>'+key+': '+val+'</li>');
         });
+        //$('.current-card').append("<ul id=\"hyperlink1\">HyperLinks</ul>");
+        //$('.current-card').append('<li> HyperLinks:</li>');
+        //$('.current-card').append('<li><a href=\"url\">http://flybase.org/reports/'+data['FlyBase ID']+'</a></li>');
     });
 
     // Data is obtained from the cgi script and rendered on the first card
@@ -19,6 +22,7 @@ $(document).ready( function() {
         $("#intro-button").css("visibility", "hidden").remove();
         $("#next-button").css("visibility", "visible");
         $(".current-card").fadeIn(1000);
+        $("#odorbase_search").fadeIn(1000);
         console.log(selector,index);
     });
 
@@ -32,7 +36,7 @@ $(document).ready( function() {
         $.getJSON('./get_random_sql.cgi', function(data) {
           //console.log(data); //DEBUG
           $.each( data, function( key, val ) {
-            $("#card"+index).append('<li>'+val+'</li>');
+            $("#card"+index).append('<li>'+key+': '+val+'</li>');
           });
         });
         $(".current-card").animate({marginLeft: "+=150px",rotate: "40deg"}).fadeOut(300).attr("class","card");
@@ -59,5 +63,35 @@ $(document).ready( function() {
         $("#card"+selector).attr("class","current-card");
       }
       console.log(selector,index);
+    });
+
+    $('#submit').click( function() {
+      // transforms all the form parameters into a string we can send to the server
+      var frmStr = $('#odorbase_search').serialize();
+
+      $.ajax({
+          url: './get_sql.cgi',
+          dataType: 'json',
+          data: frmStr,
+          success: function(data, textStatus, jqXHR) {
+            alert(data.match_count + " matches were found and " + data.match_count + " cards added to the bottom of the stack.");
+            $.each( data.matches, function( i, item ) {
+              index++;
+              $('.cardcontainer').append("<ul class=\"card\" id=\"card"+index+"\">");
+              $("#card"+index).append('<li>'+'FlyBase ID: '+item.FlyBaseID+'</li>');
+              $("#card"+index).append('<li>'+'Feature Type: '+item.Feature_Type+'</li>');
+              $("#card"+index).append('<li>'+'Name: '+item.Name+'</li>');
+              $("#card"+index).append('<li>'+'Symbol: '+item.Symbol+'</li>');
+              $("#card"+index).append('<li>'+'UniProt Function: '+item.UniProt_Function+'</li>');
+              $("#card"+index).append('<li>'+'Protein Family: '+item.Protein_Family+'</li>');
+            });
+          },
+          error: function(jqXHR, textStatus, errorThrown){
+              alert("Failed to perform search! textStatus: (" + textStatus +
+                    ") and errorThrown: (" + errorThrown + ")");
+          }
+      });
+      console.log(selector,index);
+      return false;  // prevents 'normal' form submission
     });
 });
